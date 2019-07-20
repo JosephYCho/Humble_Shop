366ec59cd923ab7d091e8064c5c01d5981e5af6d
Joseph Cho <jocho@ucdavis.edu>
2019-07-19 21:51:04 -0700

    hero component and design

--- humble-shop/src/components/hero/Hero.jsx
+++ humble-shop/src/components/hero/Hero.jsx
@@ -1,23 +1,59 @@
 import React from "react";
-
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import logo from "../../assets/Assets/logo.png";
 
 class Hero extends React.Component {
   render() {
     return (
-      <div>
+      <div id="hero-container">
         <header>
-          <nav>
-            <ul>
-              <li>Features</li>
-              <li>Pricing</li>
-              <li>Your Cart
-                  
-              </li>
-            </ul>
-          </nav>
+          <Navbar>
+            <Container className="navbar">
+              <div className="nav-left">
+                <img src={logo} alt="Simpatra" />
+              </div>
+
+              <div className="nav-right">
+                <ul className="nav-items">
+                  <NavItem>
+                    <NavLink className="text-dark nav-item" to="/toppings">
+                      <a href="#" >Feature</a>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="text-dark" to="/toppings">
+                      <a href="#">Pricing</a>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="text-dark" to="/toppings">
+                      <a href="#">Your Cart</a>
+                    </NavLink>
+                  </NavItem>
+                </ul>
+              </div>
+            </Container>
+          </Navbar>
         </header>
-        <div>
-          <h1><span className="bold-font">Elevate</span>Your Practice <span className="bold-font">Effectively</span></h1>
+        <div className="text-container">
+          <h1>
+            <span className="bold-font">Elevate </span>Your Practice{" "}
+            <span className="bold-font"> Effectively</span>
+          </h1>
+          <h2>
+            Add revenue to your practice. Utilize one platform and our
+            technology to take your practice to the next level. Simpatra has
+            created marketing tools and packages that work for you while you
+            care for your patients.
+          </h2>
         </div>
       </div>
     );
